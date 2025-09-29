092de803cf1123f8f39aa1190ee340de23175909
omarslik <omar.sleeq2@gmail.com>
2025-09-29 08:41:00 +0300

    todo 1

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ inside the <p> element with id="t1-msg".
 💡 Hint:
 document.getElementById("t1-msg").innerHTML = "Hello, World!";
 */
- 
+  const t1Msg = document.getElementById("t1-msg");
+  if (t1Msg) t1Msg.textContent = "Hello, World!";
 
 /*  
 =======================================
